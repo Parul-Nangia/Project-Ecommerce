b15991f651466c95e6d13caf3832332307c88ead
Sudhir Dadwal <sudhir.dadwal@gmail.com>
2022-09-16 14:59:16 +0530

    Login Component

--- src/components/Mainlayout.js
+++ src/components/Mainlayout.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Layout} from 'antd';
+import React, { useState } from 'react'
+import { Button, Layout} from 'antd';
 import Mainheader from './Mainheader';
+import Login from './Login';
+
 const {  Footer, Sider, Content } = Layout;
 
 
 const Mainlayout = () => {
+  const [isLogin, setIsLogin] = useState(0);
+  
+  const logout = () => {
+    localStorage.removeItem("isLogin");
+  }
   return (
     <>
-
-
+    {isLogin ? 
     <Layout style={{height:"100vh" }}>
-      <Sider>Sider</Sider>
+      <Sider>Sider
+        <Button onClick={logout}> Logout </Button>
+      </Sider>
       <Layout>
        <Mainheader />
+
         <Content>Content</Content>
         <Footer>Footer</Footer>
       </Layout>
     </Layout>
+    : 
+    <Layout>
+      <Login />
+    </Layout>
+    }
   </>
 );
 
