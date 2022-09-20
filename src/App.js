81ab79f083e18a606c93f0d1b6110b97673c623e
Harprittt <ebsharpreetkaur@gmail.com>
2022-09-20 21:30:38 +0530

    dashboard changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/Dashboard.jsx';
-import Attendance from './pages/Attendance.jsx';
-import Leave from './pages/Leave.jsx';
-import Employees from './pages/Employees.jsx';
-import Sidebar from './components/Sidebar.jsx';
-import Login from './pages/Login.js';
+import Layout from './components/Layout';
 
 
 
 
 
-const App = () => {
+
+
+function App() {
   return (
-     
-      <BrowserRouter>   
-        <Sidebar>
-          <Routes>
-            <Route path="/"element={<Login/>}/>
-           
-            <Route path="/dashboard"element={<Dashboard/>}/>
-            <Route path="/attendance"element={<Attendance/>}/>
-            <Route path="/employees"element={<Employees/>}/>
-            <Route path="/leave"element={<Leave/>}/>
-
-          </Routes>
-        </Sidebar>
-      </BrowserRouter>
-   
+      <>
+        <Layout />
+      </>
   );
-};
+}
+
 
 export default App;
