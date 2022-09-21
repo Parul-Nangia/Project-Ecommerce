0d3185c215e504e9c5b9b8263adc6f753b831ebe
parul <ebsparule362@gmail.com>
2022-09-21 18:27:52 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login  from './components/Login.js';
+import Leave from './components/Leave.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import Attendance from './components/Attendance.jsx';
+import Employees from './components/Employees.jsx';
+import Sidebar from './components/Sidebar.jsx';
 
 const App = () => {
   return (
