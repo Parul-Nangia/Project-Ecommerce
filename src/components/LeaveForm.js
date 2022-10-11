747428a6d57f7d6ee223b53b16a129f57c7888f2
Baljeet Singh <113023797+Baljeet975@users.noreply.github.com>
2022-10-11 17:06:17 +0530

    Leave Form list api

--- src/components/LeaveForm.js
+++ src/components/LeaveForm.js
@@ -1,131 +1,116 @@
-import React , {useState} from 'react'
-import { Form, Input , Button , Row} from 'antd';
+import React from 'react'
+import { Form, Input, Button, Row } from 'antd';
 import axios from 'axios';
 import { makeStyles } from "@material-ui/core";
-import { useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+
 const useStyles = makeStyles({
 
   frmItem: {
-    padding:"10px",
-    width:"50vh"
-       
+    padding: "10px",
+    width: "50vh"
+
   },
-
-
-
-btnCenter: {
-  padding:"10px",
-  width:"50vh",
-  height:"60px",
-  fontWeight:"bold",
-  backgroundColor:"#FF4500",
-  "&:hover": {
-    borderRadius: 4,
-    backgroundColor: "white",
-    color:"black"
-  },
-},
 })
+
+
+
 const LeaveForm = () => {
   const classes = useStyles();
+  const [size, setSize] = useState('default');
+
   const navigate = useNavigate()
- 
-  const url = "http://localhost:1999/leave"
-  const [data, setData] = useState({
-          EmployeeName: "",
-          SupervisorName: "",
-          Department: "",
-          LeaveType: "",
-          LeaveDate: "",
-          ReturnDate: "",
-          TotalHoursRequested: "",
-          TotalDaysRequested: ""
-        })
-        
-        const Submit = (e)=>{
-          console.log("dh")
-          console.log(e,"kjfjf")
-           e.preventDefault();
-           axios.post(url,{
-            EmployeeName:data.EmployeeName,
-            SupervisorName:data.SupervisorName,
-            Department:data.Department,
-            LeaveType:data.LeaveType,
-            Leavedate:data.LeaveDate,
-            Returndate:data.ReturnDate,
-            TotalHoursRequested:parseInt(data.TotalHoursRequested),
-            TotalDaysRequested:parseInt(data.TotalDaysRequested)
-           })
-           .then(res => {
-              console.log(res.data)
-           })
-        }
-        
-         function handle(e) {
-          const newdata={...data}
-          newdata[e.target.id] = e.target.value
-          setData(newdata)
-          console.log(newdata)
-    
-         }
-    
-         function handleClick() {
-          navigate("/dashboard")
-           }
-      
+  const [state, setState] = useState([]);
+  const [EmployeeName, setEmployeeName] = useState("");
+  const [SupervisorName, setSupervisorName] = useState("");
+  const [Department, setDepartment] = useState("");
+  const [LeaveType, setLeaveType] = useState("");
+  const [LeaveDate, setLeaveDate] = useState("");
+  const [ReturnDate, setReturnDate] = useState("")
+  const [TotalHoursRequested, setTotalHoursRequested] = useState("");
+  const [TotalDaysRequested, setTotalDaysRequested] = useState("")
+
+
+
+
+
+  function applyLeave() {
+    console.warn({ EmployeeName, SupervisorName, Department, LeaveType, LeaveDate, ReturnDate, TotalHoursRequested, TotalDaysRequested });
+    let data = { EmployeeName, SupervisorName, Department, LeaveType, LeaveDate, ReturnDate, TotalHoursRequested, TotalDaysRequested }
+
+
+    fetch("http://localhost:1999/leave", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then((Leave) => {
+      console.log("result", Leave);
+      window.alert("Leave Applied")
+
+    })
+
+  }
+
+
+  function handleClick() {
+    navigate("/dashboard")
+  }
+
   return (
     <>
- 
-    <Row justify="center" style={{ padding: "10%" ,marginLeft:"120px"}}>
 
-  <Form >
+      <Row justify="center" style={{ padding: "10%", marginLeft: "120px" }}>
+
+        <Form >
 
 
-  <Form.Item rules={[{ required: true }]} >
-    <Input onChange = {(e)=> handle(e)} value= {data.EmployeeName}  id="EmployeeName"  placeholder="Employee Name" className={classes.frmItem}  />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]} >
+            <Input onChange={(e) => { setEmployeeName(e.target.value) }} id="EmployeeName" className={classes.frmItem} placeholder="Employee Name" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true }]} >
-    <Input  onChange = {(e)=> handle(e)} value= {data.SupervisorName }  id="SupervisorName" placeholder="Supervisor Name" className={classes.frmItem}  />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]} >
+            <Input onChange={(e) => { setSupervisorName(e.target.value) }} id="SupervisorName" className={classes.frmItem} placeholder="Supervisor Name" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true }]} >
-    <Input  onChange = {(e)=> handle(e)}  value= {data.Department} id="Department"  placeholder="Department" className={classes.frmItem}  />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]} >
+            <Input onChange={(e) => { setDepartment(e.target.value) }} id="Department" className={classes.frmItem} placeholder="Department" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true }]}>
-    <Input onChange = {(e)=> handle(e)}  value= {data.LeaveType} id="LeaveType" placeholder="Leave Type"  className={classes.frmItem} />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]}>
+            <Input onChange={(e) => { setLeaveType(e.target.value) }} id="LeaveType" className={classes.frmItem} placeholder="Leave Type" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true }]}>
-    <Input  onChange = {(e)=> handle(e)} value= {data.LeaveDate} id="LeaveDate" type="calendar" placeholder="Leave Date" className={classes.frmItem} />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]}>
+            <Input onChange={(e) => { setLeaveDate(e.target.value) }} id="LeaveDate" className={classes.frmItem} type="calendar" placeholder="Leave Date" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true }]}>
-    <Input  onChange = {(e)=> handle(e)} value= {data.ReturnDate} id="ReturnDate" type="calendar" placeholder="Return Date" className={classes.frmItem} />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]}>
+            <Input onChange={(e) => { setReturnDate(e.target.value) }} id="ReturnDate" className={classes.frmItem} type="calendar" placeholder="Return Date" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true }]}>
-    <Input onChange = {(e)=> handle(e)} value= {data.TotalHoursRequested} id="TotalHoursRequested" type="number" placeholder="Total Hours Requested"  className={classes.frmItem} />
-  </Form.Item>
+          <Form.Item rules={[{ required: true }]}>
+            <Input onChange={(e) => { setTotalHoursRequested(e.target.value) }} className={classes.frmItem} id="TotalHoursRequested" type="number" placeholder="Total Hours Requested" />
+          </Form.Item>
 
-  <Form.Item rules={[{ required: true}]}>
-    <Input onChange = {(e)=> handle(e)}  value= {data.TotalDaysRequested}  id="TotalDaysRequested"  type="number"  placeholder="Total Days Requested"  className={classes.frmItem} />
-    
-  </Form.Item>
-  <Form.Item>
-    
-    <Button htmlType="submit" onclick= {(e)=> Submit(data)} className={classes.btnCenter}>Submit</Button><br/>
-    <Button onclick={handleClick} className={classes.btnCenter}>Back</Button><br />
-  </Form.Item>
-</Form>
-</Row>
-      
-  
+          <Form.Item rules={[{ required: true }]}>
+            <Input onChange={(e) => { setTotalDaysRequested(e.target.value) }} className={classes.frmItem} id="TotalDaysRequested" type="number" placeholder="Total Days Requested" />
+
+          </Form.Item>
+          <Form.Item>
+
+            <Button type="primary" size={size} onClick={applyLeave} >Submit</Button><br />
+            <Button type="primary" size={size} onClick={handleClick} >Back</Button><br />
+          </Form.Item>
+        </Form>
+      </Row>
+
+
     </>
   )
 }
 
 export default LeaveForm;
-
-
