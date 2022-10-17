7583296e5a1a100bfa99e6bce21f2d726643b041
Prince Kahol <kaholprince4620@gmail.com>
2022-10-17 14:01:53 +0530

    drop

--- src/components/Drop.js
+++ src/components/Drop.js
@@ -1,43 +1,11 @@
 import { DownOutlined } from '@ant-design/icons';
-<<<<<<< Updated upstream
-import { Dropdown, Menu, Space ,Input} from 'antd';
-import React,  { useEffect,useState} from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import {LoginOutlined,SettingTwoTone,InfoCircleTwoTone} from '@ant-design/icons';
-import {useDispatch,useSelector} from 'react-redux';
-import userEvent from '@testing-library/user-event';
-import axios from 'axios';
-
-=======
 import { Dropdown, Menu, Space } from 'antd';
 import React,  { useEffect, useState} from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import {LoginOutlined,SettingTwoTone,InfoCircleTwoTone} from '@ant-design/icons';
 import axios from 'axios';
->>>>>>> Stashed changes
 
 const Drop = () => {
-
-  
-
-  const user=useSelector(selectUser);
-  const[name,setName]=useState('');
-  
-
-  useEffect(()=>{
-    (async ()=>{
-      const{data}=await axios.get("http://localhost:1999/user/login");
-
-      setName(data.name);
-    })();
-
-  },[]);
-
-
-  
-
-
-  
   const navigate=useNavigate();
   const [open, setOpen] = useState(false);
 
@@ -99,11 +67,7 @@ const Drop = () => {
     <Dropdown overlay={menu} onOpenChange={handleOpenChange} open={open}>
       <a onClick={(e) => e.preventDefault()}>
         <Space style={{color:"black",float:"right"}}>
-<<<<<<< Updated upstream
-        <h3>Hi {name}</h3>
-=======
           <h3>Hii{name}</h3>
->>>>>>> Stashed changes
           <DownOutlined />
         </Space>
       </a>
