2bc63f223c4d923b19c1590dae9b3ca9b7770399
parul <ebsparule362@gmail.com>
2022-10-27 17:49:04 +0530

    changes

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -9,27 +9,25 @@ const Clock = () => {
   // const[minutes,setMinutes] = useState(0)
 
   const [attendance, setAttendance] = useState([]);
-  // const[disable,setDisable]=useState(false)
+  const[show,setShow]=useState(false)
+ 
 
+  // var timer;
+  // useEffect(() => {
 
-// var timer;
-// useEffect(() => {
+  //   timer= setInterval(()=>{
 
+  //     setSeconds(seconds+1);
 
-//   timer= setInterval(()=>{
+  //     if(seconds===59){
+  //       setMinutes(minutes+1);
+  //       setSeconds(0);
+  //     }
 
-//     setSeconds(seconds+1);
+  //   },1000)
 
-//     if(seconds===59){
-//       setMinutes(minutes+1);
-//       setSeconds(0);
-//     }
-
-//   },1000)
-
-// return () => clearInterval(timer);
-// });
-
+  // return () => clearInterval(timer);
+  // });
 
   const refreshClock = () => {
     setDate(new Date());
@@ -59,16 +57,15 @@ const Clock = () => {
       .then((res) => {
         setAttendance(res?.data?.attendanceData);
         console.log("Logged In Employee Attendance", attendance);
-       
-        console.log("attendance empid", attendance[0].emp_id);
-        
-       
+
+        console.log("attendance checkin", attendance.CheckIn);
       });
 
-    if (attendance[0].emp_id == decoded._id) {
+
+
+    if (attendance.CheckIn == new Date()) {
       alert("You have already checked in");
-    } 
-    else {
+    } else {
       const CheckIn = new Date();
       console.log("I am here Clock Date", CheckIn);
       const CheckOut = "";
@@ -89,7 +86,6 @@ const Clock = () => {
         });
     }
   };
-  
 
   const employeecheckout = async () => {
     const token = localStorage.getItem("access_token1");
@@ -101,73 +97,86 @@ const Clock = () => {
     const CheckOut = new Date();
     const Break = "";
     const Resume = "";
-   
-    await axios.put(`http://localhost:1999/attendance/${decoded._id}`, { 
-    CheckIn,
-    CheckOut,
-    Break,
-    Resume, 
-  })
-  .then((res) => {
-    console.log("id",decoded._id)
 
-    console.log("employee check out",res)
-  });
-   
+    await axios
+      .put(`http://localhost:1999/attendance/${decoded._id}`, {
+        CheckIn,
+        CheckOut,
+        Break,
+        Resume,
+      })
+      .then((res) => {
+        console.log("id", decoded._id);
 
+        console.log("employee check out", res);
+      });
   };
+
+
+
 
   const employeebreak = async () => {
     const token = localStorage.getItem("access_token1");
     console.log("token from local storage:", token);
     var decoded = jwt_decode(token);
     console.log("Decoded token data", decoded);
-    
+    var chkin =  attendance?.CheckIn
+    console.log("checkin",chkin)
 
-    
-    const CheckIn = "";
+
+    const CheckIn ="";
     const CheckOut = "";
     const Break = new Date();
     const Resume = "";
-   
-    await axios.put(`http://localhost:1999/attendance/${decoded._id}`, {
-    CheckIn,
-    CheckOut,
-    Break,
-    Resume, 
-  })
-  .then((res) => {
-    console.log("id",decoded._id)
 
-    console.log("employee break",res)
-    // setDisable(true)
-  });
-} 
+    await axios
+      .put(`http://localhost:1999/attendance/${decoded._id}`, {
+        CheckIn,
+        CheckOut,
+        Break,
+        Resume,
+      })
+      .then((res) => {
+        console.log("id", decoded._id);
 
-const employeeresume = async () => {
-  const token = localStorage.getItem("access_token1");
-  console.log("token from local storage:", token);
-  var decoded = jwt_decode(token);
-  console.log("Decoded token data", decoded);
+        console.log("employee break", res);
+        
+      });
+      setShow(!show)
+      // show?
+      
+  };
 
-  
-  const CheckIn = "";
-  const CheckOut = "";
-  const Break ="";
-  const Resume = new Date();
+
+
  
-  await axios.put(`http://localhost:1999/attendance/${decoded._id}`, {
-  CheckIn,
-  CheckOut,
-  Break,
-  Resume, 
-})
-.then((res) => {
-  console.log("id",decoded._id)
+  // const employeeresume = async () => {
+  //   const token = localStorage.getItem("access_token1");
+  //   console.log("token from local storage:", token);
+  //   var decoded = jwt_decode(token);
+  //   console.log("Decoded token data", decoded);
 
-  console.log("employee resume",res)
-});
-} 
+  //   const CheckIn = "";
+  //   const CheckOut = "";
+  //   const Break = "";
+  //   const Resume = Date();
+
+  //   await axios
+  //     .put(`http://localhost:1999/attendance/${decoded._id}`, {
+  //       CheckIn,
+  //       CheckOut,
+  //       Break,
+  //       Resume,
+  //     })
+  //     .then((res) => {
+  //       console.log("id", decoded._id);
+
+  //       console.log("employee resume", res);
+  //       setDisabled(false);
+  //     });
+  // };
+
+ 
   return (
     <>
       <div>
@@ -187,15 +196,44 @@ const employeeresume = async () => {
         <h1>Timer</h1>
         <h1>{minutes<10? "0"+minutes:minutes}:{seconds<10? "0"+seconds: seconds}</h1>
       </div> */}
-      {/* <Button disabled={disable} onClick={() => {employeebreak()}}>Break</Button> */}
-       
-      <div>
-        <Button style={{color:"white" ,backgroundColor:"Green",fontWeight:"Bold"}} onClick={() => {employeecheckin()}}>Checkin</Button>
-        <Button style={{color:"white" ,backgroundColor:"Tomato",fontWeight:"Bold"}} onClick={() => {employeebreak()}}>Break</Button>
-        <Button style={{color:"white" ,backgroundColor:"Red",fontWeight:"Bold"}} onClick={() => {employeeresume()}}>Resume</Button>
-        <Button style={{color:"white" ,backgroundColor:"Orange",fontWeight:"Bold"}} onClick={() => {employeecheckout()}}>Checkout</Button>
-      </div> 
 
+      <div>
+        {/* <Button style={{color:"white" ,backgroundColor:"yellow",fontWeight:"Bold"}} onClick={() => {employeereturn()}}>Return</Button> */}
+        <Button
+          style={{
+            color: "white",
+            backgroundColor: "Green",
+            fontWeight: "Bold",
+          }}
+          onClick={() => {
+            employeecheckin();
+          }}
+        >
+          Checkin
+        </Button>
+        <Button style={{color: "white" ,backgroundColor: "Tomato",fontWeight: "Bold"}} onClick={() => {employeebreak()}}>Break/Resume </Button>
+        {/* <Button
+          disabled={false}
+          style={{ color: "white", backgroundColor: "Red", fontWeight: "Bold" }}
+          onClick={() => {
+            employeeresume();
+          }}
+        >
+          Resume
+        </Button> */}
+        <Button
+          style={{
+            color: "white",
+            backgroundColor: "Orange",
+            fontWeight: "Bold",
+          }}
+          onClick={() => {
+            employeecheckout()
+          }}
+        >
+          Checkout
+        </Button>
+      </div>
     </>
   );
 };
