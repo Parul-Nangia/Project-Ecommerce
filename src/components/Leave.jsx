66c2eea826c64854754043c5f09f8c51fd4f4c9d
Harprittt <ebsharpreetkaur@gmail.com>
2022-10-27 09:54:03 +0530

    changes

--- src/components/Leave.jsx
+++ src/components/Leave.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import LeaveCards from '../components/LeaveCards';
 import LeaveTable from '../components/LeaveTable';
 import LeaveCalendar from '../components/LeaveCalendar';
@@ -16,90 +16,67 @@ const Leave = (props) => {
 
 
   const navigate = useNavigate();
-  const[view,setView]=useState(false)
-  const[name,setName]=useState("");
-  
+  const [view, setView] = useState(false)
+  const [name, setName] = useState("");
+
   useEffect(() => {
     userData();
 
   }, [])
 
-  
 
-  
-  const userData = ()=> {
+
+
+  const userData = () => {
     const token = localStorage.getItem("access_token1");
     console.log("token from local storage:", token)
     // let token = token;
     var decoded = jwt_decode(token);
-    console.log("Decoded token data",decoded);
+    console.log("Decoded token data", decoded);
     setName(decoded)
   }
 
-  if (name.role === "admin"){
-    console.log("my role is " ,name.role)
+  if (name.role === "admin") {
+    console.log("my role is ", name.role)
     return (
 
       <>
-    
-           <LeaveCards />  
-           <Link to="/leaveform"></Link>
-              <br/>
-              <Button  style={{backgroundColor:"Coral",color:"white",fontWeight:"bold"}} onClick={()=> navigate('/leaveform')}>Apply Leave</Button>
-              
-            <h1>{view}</h1>
-           <Button style={{backgroundColor:"DarkSlateGray",color:"white",fontWeight:"bold"}} onClick={()=> setView(!view)}>Calendar View</Button>
-           <Button style={{backgroundColor:"CornflowerBlue",color:"white",fontWeight:"bold"}} onClick={()=> setView(!view)}>Table iew</Button>
-          
-           {view ? <LeaveTable/> :<LeaveCalendar/>  }
 
-                
-                
 
-    </>
+        <h1>{view}</h1>
+        <Button style={{ backgroundColor: "DarkSlateGray", color: "white", fontWeight: "bold" }} onClick={() => setView(!view)}>Calendar View</Button>
+        <Button style={{ backgroundColor: "CornflowerBlue", color: "white", fontWeight: "bold" }} onClick={() => setView(!view)}>Table View</Button>
 
-    
-  )
+        {view ? <LeaveTable /> : <LeaveCalendar />}
+
+
+
+
+      </>
+
+
+    )
   }
-  if(name.role==="employee"){
-    return(
+  if (name.role === "employee") {
+    return (
       <>
-      <LeaveCards />
-      <LeaveForm />
+        <LeaveCards />
+      
+        <Link to="/leaveform"></Link>
+        <br />
+        <Button style={{ backgroundColor: "Coral", color: "white", fontWeight: "bold" }} onClick={() => navigate('/leaveform')}>Apply Leave</Button>
+
+       
       </>
     )
-  
+
   }
-  else{
-   
-    
+  else {
+
+
   }
-
-
-
-
-  return (
-
-    <>
-    
-           <LeaveCards />  
-           <Link to="/leaveform"></Link>
-              <br/>
-              <Button  onClick={()=> navigate('/leaveform')}>Apply Leave</Button>
-              
-            <h1>{view}</h1>
-           <Button onClick={()=> setView(!view)}>Calendarview</Button>
-           <Button onClick={()=> setView(!view)}>Tableview</Button>
-          
-           {view ? <LeaveTable/> :<LeaveCalendar/>  }
-
-                
-                
-
-    </>
-  );
 };
-      
+
 
 
 
