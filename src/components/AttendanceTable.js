e3468709905ed19191387f0998cf07dbf146a42b
ebsprincekahol <109081269+ebsprincekahol@users.noreply.github.com>
2022-11-04 17:42:31 +0530

    flter Api

--- src/components/AttendanceTable.js
+++ src/components/AttendanceTable.js
@@ -1,4 +1,3 @@
-// export default AttendanceTable;
 import { SearchOutlined } from "@ant-design/icons";
 import { Button, Input, Space, Table, DatePicker, Form } from "antd";
 import React, { useState, useEffect } from "react";
@@ -55,129 +54,139 @@ const AttendanceTable = () => {
       console.log("Attendance All Data", attendanceAllData);
     });
   };
-
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
   // useEffect(() => {
   //   getData();
   // }, []);
   // const getData = async () => {
-
-  //   const data = { startDate: startDate, endDate: endDate };
-
-  //   axios.get('http://localhost:1999/attendance', data).then(response => {
-  //     console.log(response.data);
-  //     setDataSource({
-  //       dataSource: response.data
-
-  //     });
-  //     console.log("startDate endDate response", dataSource);
-
+  //   await axios.get("http://localhost:1999/attendance").then((res) => {
+  //     console.log(res, "kj");
+  //     setDataSource(res?.data?.attendanceRecord);
+  //     console.log(dataSource, "data");
   //   });
-
-  //   await axios
-  //     .get(`http://localhost:1999/attendance`, {
-  //       startDate,
-  //       endDate,
-  //     })
-  //     .then((res) => {
-  //       setQueryData(res?.data?.attendanceRecord);
-  //       console.log("attendance record", res);
-  //     });
-
   // };
-  // console.log("atten record", dataSource);
-  // const onChange = (date, dateString) => {
-  //   console.log(date, dateString);
-  // };
-  // const Reset = (clearFilters) => {
-  //   clearFilters();
-  //   setSearchDate('');
-  // }
-  //   const getColumnSearchProps = (dataIndex) => ({
-  //     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, }) => (
-  //       <div style={{ padding: 8, }}>
-  //         <Input
-  //           placeholder={`Search ${dataIndex}`}
-  //           value={selectedKeys[0]}
-  //           onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-  //           onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-  //           style={{ marginBottom: 8, display: 'block', }} />
-  //         <Space>
-  //           <Button
-  //             type="primary"
-  //             onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-  //             icon={<SearchOutlined />}
-  //             size="small" style={{  width: 90,  }}  >
-  //             Search
-  //           </Button>
-  //           <Button
-  //             onClick={() => clearFilters && handleReset(clearFilters)}
-  //             size="small"
-  //             style={{
-  //               width: 90,
-  //             }}
-  //           >
-  //             Reset
-  //           </Button>
-  //           <Button
-  //           type="link"
-  //           size="small"
-  //           onClick={() => {confirm({closeDropdown: false,});setSearchText(selectedKeys[0]);setSearchedColumn(dataIndex);
-  //             }}>
-  //             Filter
-  //           </Button>
-  //         </Space>
-  //       </div>
-  //     ),
-  //     filterIcon: (filtered) => (
-  //       <SearchOutlined
-  //         style={{
-  //           color: filtered ? '#1890ff' : undefined,
-  //         }}
-  //       />
-  //     ),
-  //     onFilter: (value, record) =>
-  //     record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-  //     render: (text) =>
-  //     searchedColumn === dataIndex ? (
-  //       <Highlighter
-  //         highlightStyle={{
-  //           backgroundColor: '#ffc069',
-  //           padding: 0,
-  //         }}
-  //         searchWords={[searchText]}
-  //         autoEscape
-  //         textToHighlight={text ? text.toString() : ''}
-  //       />
-  //     ) : (
-  //       text
-  //     ),
-  // });
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+    console.log(selectedKeys[0],"pp")
+    console.log(dataIndex,"bv")
+    const payload={selectedKeys}
+    // console.log(payload,"lll")
+    // const payload =selectedKeys[0]
+    // const a={}
+    // a.append(payload)
+    // console.log(a)
+    // const payload="sudhir"
+    // axios.get("http://localhost:1999/attendance/employeeName",{name:payload}).then((res) => {
+    //   console.log(res, "kj");
+    //   setDataSource(res?.data?.attendanceDataByEmpID.selectedKeys[0]);
+    //   console.log(dataSource, "data");
+    // });
+
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters,  }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      >
+        <Input
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+      onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex]
+            .toString()
+            .toLowerCase()
+            .includes(value.toLowerCase())
+        : false,
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
   const columns = [
     {
-      title: "Employee Name",
+      title: "Name",
       dataIndex: "name",
       key: "name",
       width: "150px",
-      // ...getColumnSearchProps('name'),
+      ...getColumnSearchProps('name'),
     },
     {
       title: "CheckIn",
       dataIndex: "CheckIn",
       key: "CheckIn",
       width: "150px",
-
-      //     onFilter:(value,record)=>{
-      //       return (
-      //         record[CheckIn]
-      //         ? record[CheckIn]
-      //             .toString()
-      //             .toLowerCase()
-      //             .includes(value.toLowerCase())
-      //         : ""
-
-      //       )
-      //  },
     },
     {
       title: "CheckOut",
@@ -200,7 +209,7 @@ const AttendanceTable = () => {
 
   return (
     <>
-      <Form>
+       <Form>
         <Form.Item>
           <DatePicker
             onChange={(e) => {
@@ -244,77 +253,5 @@ const AttendanceTable = () => {
 
 export default AttendanceTable;
 
-// const [filterDate, setFilterDate] = useState("");
 
-// const [state, setState] = useState([]);
 
-// const [attendancedata, setAttendanceData] = useState([]);
-
-// useEffect(() => {
-//   employeetime();
-// }, []);
-
-// const employeetime = (emp_id) => {
-//   fetch(`http://localhost:1999/attendance/emp_id`)
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       let emptime = data.attendanceRecord;
-//       setAttendanceData(emptime);
-//       console.log("response", emptime);
-
-//     });
-// };
-// console.log(attendancedata, "hh")
-// useEffect(() => {
-//   attendancelist();
-// }, []);
-
-// const attendancelist = () => {
-//   fetch("http://localhost:1999/attendance")
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((data) => {
-//       let emp = data.userData;
-//       setState(emp);
-
-//       console.log("response", emp);
-//     });
-// };
-// console.log(state, "hh");
-
-// useEffect(() => {
-//   EmployeeDateData();
-// }, []);
-
-// const EmployeeDateData = async () => {
-//   await axios
-//     .get(`http://localhost:1999/attendance/daterange`)
-//     .then((res) => {
-//       setQuery(res?.data?.data);
-//       console.log("Date range data", res);
-//     });
-//   console.log("filter data", query);
-// };
-
-// useEffect(() => {
-//   EmployeeDateData();
-// }, []);
-
-// const EmployeeDateData = async () => {
-
-//   await axios
-//     .get(
-//       `http://localhost:1999/attendance/daterange`,
-
-//     )
-//     .then((res) => {
-//       setQuery(res?.data?.data);
-//       console.log("Date range data", res);
-
-//     });
-//     console.log("filter data", query);
-
-// }
