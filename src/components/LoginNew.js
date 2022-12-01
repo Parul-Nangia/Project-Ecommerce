0466c1529383ac9060a5b72c4da5e563068bec80
ebsprincekahol <109081269+ebsprincekahol@users.noreply.github.com>
2022-12-01 18:04:17 +0530

    show message

--- src/components/LoginNew.js
+++ src/components/LoginNew.js
@@ -1,111 +1,5 @@
-import { Button, Form, Input, Row ,message} from "antd";
-import React, { useState } from "react";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-
-
-const LoginNew = () => {
-  const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const submit = async (e) => {
-    try {
-      console.log("going forward");
-      e.preventDefault();
-      const show ={name,password}
-      console.log(show,"fghdshjgdfkjgjdfg")
-     
-      const { data } = await axios.post(`${process.env.REACT_APP_BASE_URL}/user/login`, { name, password, } )
-  //     .then(()=>{
-  // message.success("Authorized ")
-  
-  //     });
-      console.log(data,"dataaaa")
-  axios.defaults.headers.common["Authorization"] = `Bearer ${data["token"]}`;
-  localStorage.setItem("access_token1", JSON.stringify(data.token));
-  console.log(localStorage,"localStorage")
-  window.location.reload();
-  navigate("/dashboard");  
-    } 
-    catch (error) {
-      if(name==="" && password===""){
-        message.error("Fill your UserName and Password!!")
-      }
-      else{
-     message.error("Incorrect password or UserName")
-      }
-    // message.open({
-    //   type: 'error',
-    //   content: 'unauthorized',
-    //   duration: 5,
-    //   style: {
-    //     marginTop: '10vh',
-    //   },
-    // });
-     
-    }
-   
-  };
-
-  return (
-    <>
-      <Row justify="center" style={{ padding: "10%", marginTop: "20px" }}>
-        <Form style={{ width: "300px" }} 
-         autoComplete="off"
-         onFinish={submit}
-         >
-          <img className="logoimg" src="ebs.png" />
-
-          <Form.Item
-          name="ccccc"
-            rules={[{ required: true, message: "Please input your Username!" }]}
-          >
-            <Input
-              prefix={<UserOutlined />}
-              placeholder="Username"
-              onChange={(e) => setName(e.target.value)}
-            />
-          </Form.Item>
-
-          <Form.Item
-            name="Password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
-          >
-            <Input
-              type="password"
-              prefix={<LockOutlined />}
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <Button htmlType="submit" onClick={submit}  className="center-btn">
-              Login
-            </Button>
-            <br />
-          </Form.Item>
-        </Form>
-      </Row>
-    </>
-  );
-};
-
-export default LoginNew;
-
-
-
-
-
-
-
-
-
-
 // import { Button, Form, Input, Row ,message} from "antd";
-// import React, { useState,useEffect } from "react";
+// import React, { useState } from "react";
 // import { LockOutlined, UserOutlined } from "@ant-design/icons";
 // import axios from "axios";
 // import { useNavigate } from "react-router-dom";
@@ -115,15 +9,7 @@ export default LoginNew;
 //   const navigate = useNavigate();
 //   const [name, setName] = useState("");
 //   const [password, setPassword] = useState("");
-// useEffect(()=>{
-//   dataShow()
-// })
-// const dataShow =()=>{
-//   axios.get(`${process.env.REACT_APP_BASE_URL}/user`).then((res)=>{
-//     console.log(res,"Response")
-//     console.log(res?.data?.userData,"Response")
-//   })
-// }
+
 //   const submit = async (e) => {
 //     try {
 //       console.log("going forward");
@@ -144,7 +30,12 @@ export default LoginNew;
 //   navigate("/dashboard");  
 //     } 
 //     catch (error) {
-//      message.error("Unauthorized")
+//       if(name==="" && password===""){
+//         message.error("Fill your UserName and Password!!")
+//       }
+//       else{
+//      message.error("Incorrect password or UserName")
+//       }
 //     // message.open({
 //     //   type: 'error',
 //     //   content: 'unauthorized',
@@ -191,7 +82,7 @@ export default LoginNew;
 //           </Form.Item>
 
 //           <Form.Item>
-//             <Button htmlType="submit"  className="center-btn">
+//             <Button htmlType="submit" onClick={submit}  className="center-btn">
 //               Login
 //             </Button>
 //             <br />
@@ -203,3 +94,147 @@ export default LoginNew;
 // };
 
 // export default LoginNew;
+
+
+
+
+
+
+
+
+
+
+import { Button, Form, Input, Row ,message} from "antd";
+import React, { useState,useEffect } from "react";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
+const LoginNew = () => {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [correctData,setCorrectData] = useState("");
+useEffect(()=>{
+  dataShow()
+},[])
+const dataShow =()=>{
+  axios.get(`${process.env.REACT_APP_BASE_URL}/user`).then((res)=>{
+    console.log(res,"Response")
+    console.log(res?.data?.userData,"Response")
+    const apnatime=res?.data?.userData
+    console.log(apnatime)
+    setCorrectData(apnatime)
+  })
+}
+  const submit = async (e) => {
+    console.log(correctData,"show data")
+    try {
+      console.log("going forward");
+      e.preventDefault();
+      const show ={name,password}
+      console.log(show,"Show")
+
+      const { data } = await axios.post(`${process.env.REACT_APP_BASE_URL}/user/login`, { name, password, } )
+  //     .then(()=>{
+  // message.success("Authorized ")
+  
+  //     });
+      console.log(data,"dataaaa")
+  axios.defaults.headers.common["Authorization"] = `Bearer ${data["token"]}`;
+  localStorage.setItem("access_token1", JSON.stringify(data.token));
+  console.log(localStorage,"localStorage")
+  message.success("!Login")
+  window.location.reload();
+  navigate("/dashboard");  
+    } 
+    catch (error) {
+  console.log(correctData,"show data catch")
+  const text =(item)=>{
+  return [item.name].join(" ");
+  }
+  const text1 =(item)=>{
+    return [item.password].join(" ");
+    }
+  const UserName=(correctData.map(text))
+  const Password=(correctData.map(text1))
+  console.log(correctData)
+if(name==="" && password===""){
+  message.error("Fill your username and password")
+}
+else if(!Password.includes(password) && !UserName.includes(name)){
+    message.error("InValid password & username!!")
+}
+else if(!UserName.includes(name)){
+      console.log("Incorrect userName")
+      message.error("Incorrect UserName!")
+}
+else if(!Password.includes(password)){
+        console.log("Incorrect Password")
+        message.error("Incorrect password!")
+  }
+
+
+   
+    // else{
+    //   console.log("Incorrect Password")
+    // }
+    // message.open({
+    //   type: 'error',
+    //   content: 'unauthorized',
+    //   duration: 5,
+    //   style: {
+    //     marginTop: '10vh',
+    //   },
+    // });
+     
+    }
+   
+  };
+
+  return (
+    <>
+      <Row justify="center" style={{ padding: "10%", marginTop: "20px" }}>
+        <Form style={{ width: "300px" }} 
+         autoComplete="off"
+         >
+          <img className="logoimg" src="ebs.png" />
+
+          <Form.Item
+          name="Name"
+            rules={[{ required: true, message: "Please input your Username!" }]}
+          >
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Username"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="Password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input
+              type="password"
+              prefix={<LockOutlined />}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button htmlType="submit" onClick={submit}  className="center-btn">
+              Login
+            </Button>
+            <br />
+          </Form.Item>
+        </Form>
+      </Row>
+    </>
+  );
+};
+
+export default LoginNew;
+
