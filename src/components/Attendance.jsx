b1dad72be6df94e709dcc2d419dde2b4dc56bee4
Harprittt <ebsharpreetkaur@gmail.com>
2022-12-05 17:29:30 +0530

    changes

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import AttendanceCards from '../components/AttendanceCards';
 import AttendanceTable from '../components/AttendanceTable';
+import EmployeeAttendanceCalendar from '../components/EmployeeAttendanceCalendar';
 import Clock from '../components/Clock';
 import jwt_decode from 'jwt-decode';
 import Error from '../components/Error';
 import { useState, useEffect } from "react";
-
+import { FcCalendar } from "react-icons/fc";
+import { Button, Modal } from "antd";
+import { Calendar, Badge } from "antd";
+import axios from "axios";
+import {
+  ScheduleOutlined 
+} from "@ant-design/icons";
 
 
 
@@ -13,23 +20,72 @@ import { useState, useEffect } from "react";
 const Attendance = () => {
 
   const [name, setName] = useState("");
+  const [isopenmodal, setIsOpenModal] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
 
   useEffect(() => {
+    const userData = () => {
+      const token = localStorage.getItem("access_token1");
+      console.log("token from local storage:", token)
+      // let token = token;
+      var decoded = jwt_decode(token);
+      console.log("Decoded token data", decoded);
+      setName(decoded)
+    }
     userData();
 
   }, [])
 
 
+  useEffect(() => {
+    const getAllData = async () => {
+      await axios
+        .get(`${process.env.REACT_APP_BASE_URL}/attendance`)
+        .then((res) => {
+          setDataSource(res?.data?.attendanceData);
+        });
+    };
+    getAllData();
+  }, []);
 
+  const dateCellRender = (value) => {
+    // console.log("value", value)
+    const stringValue = value.format("YYYY-MM-DD");
+    var newArray = dataSource?.filter(function (el) {
 
-  const userData = () => {
-    const token = localStorage.getItem("access_token1");
-    console.log("token from local storage:", token)
-    // let token = token;
-    var decoded = jwt_decode(token);
-    console.log("Decoded token data", decoded);
-    setName(decoded)
-  }
+      return el.TodayDate === stringValue
+
+      //  el.EmployeeName &&
+      //  el._id;
+
+    }
+    )
+    console.warn("dataSource", dataSource)
+
+    return (
+      <>
+
+        {/* <ul className="events"> */}
+        {newArray.map((item) => (
+          <li key={item._id}>
+            <Badge status="success" text={item.name}/>
+          </li>
+        ))}
+        {/* </ul> */}
+
+      </>
+    )
+
+  };
+
+  const handleCancel = () => {
+    setIsOpenModal(false);
+  };
+
+  const showModal = () => {
+    setIsOpenModal(true);
+
+  };
 
   if (name.role === "admin") {
     console.log("my role is ", name.role)
@@ -47,6 +103,24 @@ const Attendance = () => {
         <br />
         <AttendanceCards />
         <br />
+        <Button
+        onClick={showModal}
+        className="calendarbtn2"
+        >
+        Calendar view{<ScheduleOutlined />}
+        </Button>
+        <br />
+        <Modal
+
+          title="All employees attendance"
+          cancelButtonProps={{ style: { display: "none" } }}
+          okButtonProps={{ style: { display: "none" } }}
+          open={isopenmodal}
+          onCancel={handleCancel}
+          width={1000}
+        >
+          <Calendar dateCellRender={dateCellRender} />
+        </Modal>
         <AttendanceTable />
 
       </>
@@ -54,15 +128,19 @@ const Attendance = () => {
   }
   if (name.role === "employee") {
     return (
-      <div style={{ display: "flex" }}>
-        <h1>DATE :</h1>
+      <>
+        <div style={{ display: "flex" }}>
+          <h1>DATE :</h1>
 
-        <div style={{ marginLeft: "5px" }}>{new Date().toLocaleDateString()}</div>
+          <div style={{ marginLeft: "5px" }}>{new Date().toLocaleDateString()}</div>
 
-        <h1 style={{ marginLeft: "15px" }}>TIME :</h1>
-        <div style={{ marginLeft: "5px" }}>{new Date().toLocaleTimeString()}</div>
-      </div>
-
+          <h1 style={{ marginLeft: "15px" }}>TIME :</h1>
+          <div style={{ marginLeft: "5px" }}>{new Date().toLocaleTimeString()}</div>
+        </div>
+        <div>
+          <EmployeeAttendanceCalendar />
+        </div>
+      </>
     )
 
   }
